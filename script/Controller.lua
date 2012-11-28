6049f899c1ee297aa62ea1139f31dae1b1772f07
Xsoda <Xsoda@live.cn>
2012-11-28 16:21:47 +0800

    format lua file

--- script/Controller.lua
+++ script/Controller.lua
@@ -3,42 +3,41 @@ require "logger"
 require "serialport"
 
 function received_hook()
-	logger.log_info("received data")
-	data = serialport.read();
-	for k,v in pairs(data) do
-		logger.log_info("key: "..k.."  | value: "..v)
-	end
-	serialport.write({0, 1, 2, 3, 4, 5})
+    logger.log_info("received data")
+    data = serialport.read();
+    for k,v in pairs(data) do
+        logger.log_info("key: "..k.."  | value: "..v)
+    end
+    serialport.write({0, 1, 2, 3, 4, 5})
 end
 function error_hook(info)
-	logger.log_info("serial error: "..info)
+    logger.log_info("serial error: "..info)
 end
 function pinchange_hook(pin)
-	logger.log_info("serial port pin changed: ".. pin)
+    logger.log_info("serial port pin changed: ".. pin)
 end
 
 function OpenSerial(arg1, arg2)
-	logger.log_info("OpenSerial -> "..arg1.." "..arg2)
-	serialport.set_error_hook(error_hook)
-	serialport.set_pinchange_hook(pinchange_hook)
-	serialport.set_received_hook(received_hook)
-	return serialport.open(arg1, arg2)
+    logger.log_info("OpenSerial -> "..arg1.." "..arg2)
+    serialport.set_error_hook(error_hook)
+    serialport.set_pinchange_hook(pinchange_hook)
+    serialport.set_received_hook(received_hook)
+    return serialport.open(arg1, arg2)
 end
 
 function CloseSerial(arg1, arg2)
-	logger.log_info("CloseSerial -> "..arg1.." "..arg2)
-	return serialport.close()
+    logger.log_info("CloseSerial -> "..arg1.." "..arg2)
+    return serialport.close()
 end
 
 
 dispatch = {
-	open_serial = OpenSerial,
-	close_serial = CloseSerial,
+    open_serial = OpenSerial,
+    close_serial = CloseSerial,
 }
 
 function Dispatch(command, arg1, arg2)
-	logger.log_error("come from lua script. command -> "..command.." arg1 -> "..arg1.." arg2 -> "..arg2)	
-	return dispatch[command](arg1, arg2)
+    logger.log_error("come from lua script. command -> "..command.." arg1 -> "..arg1.." arg2 -> "..arg2)    
+    return dispatch[command](arg1, arg2)
 end
-
 
--- script/serialport.lua
+++ script/serialport.lua
@@ -1,31 +1,30 @@
-
-serialport = {	
-	open = function (arg1, arg2)
-				local wrapper = require("luawrapper")
-				return wrapper.serialport_open(arg1, arg2)
-			end,
-	close = function ()
-				local wrapper = require("luawrapper")
-				return wrapper.serialport_close()
-			end,
-	read = function()
-			   local wrapper = require("luawrapper")
-			   return wrapper.serialport_read()
-			end,
-	write = function(data)
-				local wrapper = require("luawrapper")
-				return wrapper.serialport_write(data)
-			end,
-	set_received_hook = function (fn)
-							local wrapper = require("luawrapper")
-							return wrapper.serialport_set_received_callback(fn)
-						end,
-	set_error_hook = function (fn)
-						 local wrapper = require("luawrapper")
-						 return wrapper.serialport_set_received_callback(fn)
-					end,
-	set_pinchange_hook = function (fn)
-							 local wrapper = require("luawrapper")
-							 return wrapper.serialport_set_pinchange_callback(fn)
-						end,
+serialport = {    
+    open = function (arg1, arg2)
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_open(arg1, arg2)
+    end,
+    close = function ()
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_close()
+    end,
+    read = function()
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_read()
+    end,
+    write = function(data)
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_write(data)
+    end,
+    set_received_hook = function (fn)
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_set_received_callback(fn)
+    end,
+    set_error_hook = function (fn)
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_set_received_callback(fn)
+    end,
+    set_pinchange_hook = function (fn)
+        local wrapper = require("luawrapper")
+        return wrapper.serialport_set_pinchange_callback(fn)
+    end,
 }
